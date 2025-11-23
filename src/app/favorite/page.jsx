dc6e1de731a3983809997925b992d0b542b400fa
ashikkhan <ashikkhan314167@gmail.com>
2025-11-23 19:06:33 +0600

    add user

--- src/app/favorite/page.jsx
+++ src/app/favorite/page.jsx
@@ -15,7 +15,8 @@ export default function FavoritePage() {
     const { addToCart } = useCart(user);
     const { favorites, loading, removeFavorite } = useFavorite(user)
     console.log(favorites)
-    if(loading) return <p className='text-center pt-22'>Loading...</p>
+    if(!user) return <p className='text-center pt-22'>Please Login Fast</p>
+    if (loading) return <p className='text-center pt-22'>Loading...</p>
     return (
         <div className='pt-22'>
             <div className='grid md:grid-cols-2  lg:grid-cols-4 container mx-auto gap-4 mb-12'>
@@ -31,7 +32,7 @@ export default function FavoritePage() {
                             />
                             <div className='flex justify-between'>
                                 <h1 className='text-xl font-semibold text-gray-500'>{food.foodId.title}</h1>
-                            <button onClick={()=> removeFavorite(food?.foodId)} className='text-xl mr-3 text-red-500'><MdAutoDelete /></button>
+                                <button onClick={() => removeFavorite(food?.foodId)} className='text-xl mr-3 text-red-500'><MdAutoDelete /></button>
                             </div>
                             <Link href={`/FoodDetails/${food.foodId._id}`}>
                                 <p className="text-blue-400 flex items-center gap-1 hover:underline text-sm">
@@ -42,12 +43,12 @@ export default function FavoritePage() {
                             <div className='flex justify-between text-gray-400 mx-2'>
                                 <p className='flex items-center gap-1 text-gray-800 font-semibold'>{food.foodId.price} <TbCoinTaka /><span className='line-through text-red-400'>{food.foodId.oldPrice}</span></p>
                                 <p className='flex items-center gap-1 '><FaStar className='text-yellow-500'></FaStar>{food.foodId.rating}</p>
-                               
+
                             </div>
-                             <div className='flex justify-center mx-4'>
+                            <div className='flex justify-center mx-4'>
                                 <button onClick={() => addToCart(food.foodId)} className='flex px-4 py-2 items-center gap-2 rounded-sm bg-green-500 hover:bg-green-700 cursor-pointer text-white font-semibold'>Add to Cart <FaShoppingCart></FaShoppingCart></button>
-                                
-                             </div>
+
+                            </div>
 
                         </div>
                     ))
